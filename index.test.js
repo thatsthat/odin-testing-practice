97da4cbba69c7d2e28f00ceb0f3efb3bd09100d1
Aram Anto <aram.anto@gmail.com>
2022-10-25 09:01:42 +0200

    done

--- index.test.js
+++ index.test.js
@@ -34,12 +34,10 @@ test("apply caesar cypher", () => {
 });
 
 test("provide stats on an array of numbers", () => {
-  expect(
-    tF.analyzeArray([1, 8, 3, 4, 2, 6]).objectContaining({
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    })
-  );
+  expect(tF.analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
